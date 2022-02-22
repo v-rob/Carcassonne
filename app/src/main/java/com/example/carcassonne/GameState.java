e39cc7854725376cb6f4475063ebd85b4e0b118b
soph <annasarcangel@gmail.com>
2022-02-21 19:19:34 -0800

    Game State Class

---
+++ app/src/main/java/com/example/carcassonne/GameState.java
@@ -0,0 +1,20 @@
+package com.example.carcassonne;
+
+public class GameState {
+
+    public GameState(){
+
+    }
+
+    public GameState(GameState gs){
+
+    }
+
+    public String toString(){
+        return "hi";
+    }
+
+    //actions here
+
+
+}
