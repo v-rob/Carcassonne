4a4e631342e56a7b5005049a46370df49491d4d7
Cheyanne <yim25@up.edu>
2022-02-22 21:27:20 -0800

    player constructor comments

--- app/src/main/java/com/example/carcassonne/Player.java
+++ app/src/main/java/com/example/carcassonne/Player.java
@@ -13,12 +13,15 @@ public class Player {
     private int score;
     private String name;
 
+    //constructor for the player only taking in a string as the player's name and
+    // setting the starting number of meeples and starting score for each player
     public Player(String n){
         meepleCount = 7;
         score = 0;
         name = n;
     }
 
+    //copy constructor for the player constructor
     public Player(Player player){
         this.meepleCount = player.meepleCount;
         this.score = player.score;
