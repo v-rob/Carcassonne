dc56664e0d79182df4e86ecc20f1c8e713e0395a
Backus <backus24@up.edu>
2022-04-22 15:26:33 -0700

    Added Tiles for gameStateUnit Test Board

--- app/src/androidTest/java/com/example/carcassonne/CarcassonneGameStateTest.java
+++ app/src/androidTest/java/com/example/carcassonne/CarcassonneGameStateTest.java
@@ -26,7 +26,47 @@ public class CarcassonneGameStateTest extends TestCase {
     public void beforeRun() {
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         BitmapProvider.createInstance(appContext.getResources());
+
+        CarcassonneGameState gameState = new CarcassonneGameState(2);
+
+        Tile firstTile = new Tile('a');
+        gameState.getBoard().setTileDirectly(1,1,firstTile);
+
+        Tile secondTile = new Tile('a');
+        secondTile.setRotation(270);
+        gameState.getBoard().setTileDirectly(2,1,secondTile);
+        secondTile.setMeepleSection(secondTile.getSectionFromPosition(Tile.SIZE / 2, Tile.SIZE / 2));
+
+        Tile thirdTile = new Tile('k');
+        gameState.getBoard().setTileDirectly(1,2, thirdTile);
+
+        Tile fourthTile = new Tile('t');
+        fourthTile.setRotation(90);
+        gameState.getBoard().setTileDirectly(3,1, fourthTile);
+        fourthTile.setMeepleSection(fourthTile.getRoadSection(3));
+
+        Tile fifthTile = new Tile('e');
+        fifthTile.setRotation(270);
+        gameState.getBoard().setTileDirectly(2,2,fifthTile);
+        fifthTile.setMeepleSection(fifthTile.getSection(6));
+
+        Tile sixthTile = new Tile('e');
+        gameState.getBoard().setTileDirectly(3,2,sixthTile);
+        sixthTile.setMeepleSection(sixthTile.getSection(5));
+
+        Tile seventhTile = new Tile('e');
+        seventhTile.setRotation(180);
+        gameState.getBoard().setTileDirectly(3,1, seventhTile);
+        seventhTile.setMeepleSection(seventhTile.getSection(5));
+
+        Tile eighthTile = new Tile('e');
+        gameState.getBoard().setTileDirectly(2,1, eighthTile);
+
+        Tile ninthTile = new Tile('e');
+        gameState.getBoard().setTileDirectly(1,1, ninthTile);
+
     }
+
 
     @Test
     public void testPlaceTile() {
