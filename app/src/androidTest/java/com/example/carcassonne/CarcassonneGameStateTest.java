a8b8f04bbc804c13d1258ef63aa27dee72d285c7
soph <annasarcangel@gmail.com>
2022-04-22 16:21:05 -0700

    last unit test

--- app/src/androidTest/java/com/example/carcassonne/CarcassonneGameStateTest.java
+++ app/src/androidTest/java/com/example/carcassonne/CarcassonneGameStateTest.java
@@ -88,6 +88,58 @@ public class CarcassonneGameStateTest extends TestCase {
         assertTrue(gameState.confirmMeeple());
 
 
+        /*
+                this.textBox.clearComposingText();
+
+        //Creating instances
+        CarcassonneGameState firstInstance = new CarcassonneGameState(4);
+        CarcassonneGameState firstCopy = new CarcassonneGameState(firstInstance);
+
+        //Calls all actions
+        firstInstance.placeTile(1, 2, 1);
+        this.textBox.append("Player 1 placed the tile at (2, 1)\n");
+
+        firstInstance.rotateTile(1);
+        this.textBox.append("Player 1 rotated the current tile\n");
+
+        firstInstance.confirmTile(1);
+        this.textBox.append("Player 1 confirmed the current tile placement\n");
+
+        firstInstance.placeMeeple(1, 50, 50);
+        this.textBox.append("Player 1 placed a meeple\n");
+
+        firstInstance.confirmMeeple(1);
+        this.textBox.append("Player 1 confirmed meeple placement\n");
+
+        firstInstance.placeTile(2, 2, 2);
+        this.textBox.append("Player 2 placed the tile at (2, 2)\n");
+
+        firstInstance.confirmTile(2);
+        this.textBox.append("Player 2 confirmed the current tile\n");
+
+        firstInstance.resetTurn(2);
+        this.textBox.append("Player 2 reset turn\n");
+
+        firstInstance.confirmTile(2);
+        this.textBox.append("Player 2 confirmed the current tile\n");
+
+        firstInstance.confirmMeeple(2);
+        this.textBox.append("Player 2 confirmed meeple placement\n");
+
+        firstInstance.quitGame(3);
+        this.textBox.append("Player 3 quit the game.\n");
+
+        CarcassonneGameState secondInstance = new CarcassonneGameState(4);
+        CarcassonneGameState secondCopy = new CarcassonneGameState(secondInstance);
+
+        //Verify the copies are the same
+        this.textBox.append("\n Is first copy equal to second copy?\n");
+        this.textBox.append(" " + secondInstance.toString().equals(secondCopy.toString()));
+
+        //Print all information to screen
+        this.textBox.append("firstCopy: " + firstCopy.toString());
+        this.textBox.append("secondCopy: " + secondCopy.toString());
+         */
     }
 
 }
